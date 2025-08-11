6f76f5a5eb754ebe779480fd7355bac62d9dab0d
KrishnaChaitanya45 <kommerakrishnachaitanya@gmail.com>
2025-08-11 14:00:24 +0530

    HOTFIX: Image URL in landing page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,14 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center p-8">
+    <div className="min-h-screen flex flex-col items-center justify-center gap-8 p-8">
       <Image
-        src="/next.svg"
+        src="/logos/full-white.svg"
         alt="Logo"
-        width={100}
+        width={350}
         height={100}
         priority
       />
+      
       <h1 className="text-4xl font-bold mt-4">Coming Soon</h1>
       <p className="mt-2 text-lg text-gray-600">
         We’re working hard to launch our new site. ( You could help us by contributing! 😉 )
